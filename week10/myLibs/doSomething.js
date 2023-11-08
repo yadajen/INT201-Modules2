57944794a29e3ab959adf42b4767f7885161235c
yadajen <110714179+yadajen@users.noreply.github.com>
2023-11-08 12:33:54 +0700

    Delete week10 directory

--- week10/myLibs/doSomething.js
+++
@@ -1,6 +0,0 @@
-function doSomething(msg) {
-    return msg
-  }
-  // module.exports = doSomething
-  // module.exports = { doSomething: doSomething }
-  export { doSomething as default } //default export
