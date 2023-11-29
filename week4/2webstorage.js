0a55564bf84760e3117872aef4f4f407790a9675
yadajen <110714179+yadajen@users.noreply.github.com>
2023-11-29 12:39:53 +0700

    Add files via upload

---
+++ week4/2webstorage.js
@@ -0,0 +1,16 @@
+// let count = localStorage.getItem('counter') 
+// if(count===null) localStorage.setItem('counter',1)
+// else localStorage.setItem('counter',++count)
+
+// alert(localStorage.getItem('counter'))
+
+//session storage//refesh sessionไม่หาย //new tab//ปิดbrowser หาย
+let count = sessionStorage.getItem('counter')
+if(count===null) sessionStorage.setItem('counter',1)
+ else sessionStorage.setItem('counter',++count)
+
+alert(sessionStorage.getItem('counter'))
+
+sessionStorage.setItem('color', 'green')
+sessionStorage.removeItem('counter')
+sessionStorage.clear() //remove all sessionStorage
