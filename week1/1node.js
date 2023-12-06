5b12a5864e12630db22044c0f85ed9f037c72514
yadajen <110714179+yadajen@users.noreply.github.com>
2023-12-07 00:52:18 +0700

    Update 1node.js

--- week1/1node.js
+++ week1/1node.js
@@ -63,5 +63,5 @@ console.log(headElement.nextSibling)
 //get ชุด attibute ของtagที่เราสนใจ
 const headFirstElementChild = headElement.firstElementChild
 const headFirstElementChildAttrs = headFirstElementChild.attributes
-console.log(headFirstElementChildAttrs)//{0: charset, charset: charset,length:1}
-Array.from(headFirstElementChildAttrs).forEach((attr) => console.log(attr))
+console.log(headFirstElementChildAttrs)//{0: charset, 1: id,charset: charset,id: id,length:2}
+Array.from(headFirstElementChildAttrs).forEach((attr) => console.log(attr))//chaset="UTF-8". //id="meta-1"
