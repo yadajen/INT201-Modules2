b3c4d0263bd9881dcf24630993fa80f7e4f95190
yadajen <110714179+yadajen@users.noreply.github.com>
2023-12-07 09:07:29 +0700

    Update 3systemDialog.js

--- week2/libs/3systemDialog.js
+++ week2/libs/3systemDialog.js
@@ -1,5 +1,5 @@
 window.alert('Do you want to exit')//แจ้งข้อความอย่างเดียว//ok
-const username = window.prompt('Plasee enter your name', 'Ex.,Yada jenr')//ให้user กรอกข้อมูลได้ และเราสามารถเอาค่าที่userกรอกเก็บไว้ใช้ต่อได้ //ok and cancle
+const username = window.prompt('Plasee enter your name', 'Ex.,Yada jenr')//ให้user กรอกข้อมูลได้ และเราสามารถเอาค่าที่userกรอกเก็บไว้ใช้ต่อได้ //ok return name value and cancle return null
 console.log(username)
 const userAction = window.confirm(`Good Bye, ${username}`)//return true, false
 console.log(userAction)
